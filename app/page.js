d5f79a4e093361cad2b7c3fce94274c56d3b8e54
storeskhaliil <storeskhaliil@gmail.com>
2025-12-16 19:45:37 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,19 @@
-export default function HomePage() {
-  return null;
+// app/page.js
+import Link from "next/link";
+import { products } from "@/data/products";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>My Shop</h1>
+
+      {products.map((p) => (
+        <div key={p.id}>
+          <h2>{p.name}</h2>
+          <p>${p.price}</p>
+          <Link href={`/product/${p.id}`}>View</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
