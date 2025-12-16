41ac9c874789a581b3c6c10d4a2981a8bf0c8cce
storeskhaliil <storeskhaliil@gmail.com>
2025-12-16 19:45:14 +0200

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,14 @@
+export const products = [
+  {
+    id: 1,
+    name: "T-Shirt",
+    price: 20,
+    image: "/shirt.jpg",
+  },
+  {
+    id: 2,
+    name: "Hoodie",
+    price: 40,
+    image: "/hoodie.jpg",
+  },
+];
