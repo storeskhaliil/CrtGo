9e23f3ad2786227ae5dbeb12627a2832b654a451
storeskhaliil <storeskhaliil@gmail.com>
2025-12-16 20:11:15 +0200

    Remove CartProvider from Providers component
    Removed CartProvider from Providers component.

--- app/providers.js
+++ app/providers.js
@@ -2,14 +2,11 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/theme";
-import { CartProvider } from "@/context/CartContext";
 
 export default function Providers({ children }) {
   return (
     <ChakraProvider theme={theme}>
-      <CartProvider>
         {children}
-      </CartProvider>
     </ChakraProvider>
   );
 }
