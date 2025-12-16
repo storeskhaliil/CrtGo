0d7c1eaef94f29947f1bad787e906cbbe09aab31
storeskhaliil <storeskhaliil@gmail.com>
2025-12-16 20:10:48 +0200

    Delete app/admin directory

--- app/admin/layout.js
+++
@@ -1,9 +0,0 @@
-// app/admin/layout.js
-import { redirect } from "next/navigation";
-
-export default function AdminLayout({ children }) {
-  if (process.env.ADMIN_SECRET !== "my-secret") {
-    redirect("/");
-  }
-  return children;
-}
