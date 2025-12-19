8c14bee3a6dbd289844ca6b882772da4d8b654bf
storeskhaliil <storeskhaliil@gmail.com>
2025-12-19 14:27:32 +0200

    Update providers.js

--- app/providers.js
+++ app/providers.js
@@ -1,12 +1,19 @@
+// app/providers.js
 "use client";
 
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "@/theme";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function Providers({ children }) {
+const queryClient = new QueryClient();
+
+export function Providers({ children }) {
   return (
-    <ChakraProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
         {children}
-    </ChakraProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
