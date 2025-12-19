06cd23cda677f6c849515cd5a9003b521203a483
storeskhaliil <storeskhaliil@gmail.com>
2025-12-19 23:36:11 +0200

    Update Overlay.jsx

--- components/menu1/Overlay.jsx
+++ components/menu1/Overlay.jsx
@@ -76,6 +76,7 @@ export default function Overlay({ isOpen, onClose, item }) {
         <Box
           flexShrink={0}
           w={{ base: "100%", lg: "40%" }}
+        maxW={{ base: "100%", md: "300px", lg: "40%" }}
           aspectRatio="1 / 1"
           order={{ base: 1, lg: 2 }}
         >
